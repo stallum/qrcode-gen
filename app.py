5f7b7fc708d7ddfff9a5e06034d99ca746eab68f
Artur <stallumgp8@gmail.com>
2025-08-13 19:30:34 -0300

    adicionando funcionamento geral do programa

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 # esse projeto tem objetivo criar uma ferramenta pessoal de uso próprio
 import qrcode
+from PIL import Image
 
 class createQrCode():
     
-    def link():
-        link = input('insira um link aqui: ')
-        return link
+    @staticmethod
+    def form():
+        link = input('insira o link do seu qrcode aqui: \n')
+        name = input('insira aqui que nome você quer dar a seu QrCode: \n')
+        return link, name
     
-    def criarQrcode(link):
-        
-        pass
+
+    def criarQrcode(link, name):
+        img = qrcode.make(link)
+
+        img.save(f'{name}.png')
+
+if __name__ == "__main__":
+    
+    link_do_qrcode, nome_do_arquivo = createQrCode.form()
+    createQrCode.criarQrcode(link_do_qrcode, nome_do_arquivo)
+    print(f"QR Code '{nome_do_arquivo}.png' criado com sucesso!")
