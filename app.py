30f19ee2a032ad2992d1a17bb5adc727b6ef6c08
Artur <arturgab008@gmail.com>
2025-08-13 17:52:34 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# esse projeto tem objetivo criar uma ferramenta pessoal de uso próprio
+import qrcode
+
+class createQrCode():
+    
+    def link():
+        link = input('insira um link aqui: ')
+        return link
+    
+    def criarQrcode(link):
+        
+        pass
