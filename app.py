fa252bde76073223148b31a9b228e49be68c1f08
Artur <arturgab008@gmail.com>
2025-08-14 07:19:28 -0300

    creating function for save qrCode

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 # esse projeto tem objetivo criar uma ferramenta pessoal de uso próprio
 import qrcode
 from PIL import Image
+import os
+import re
 
 class createQrCode():
-    
-    @staticmethod
+
     def form():
         link = input('insira o link do seu qrcode aqui: \n')
         name = input('insira aqui que nome você quer dar a seu QrCode: \n')
-        return link, name
+        name_treted = createQrCode.tratamentoNome(name=name)
+        return link, name_treted
     
 
-    def criarQrcode(link, name):
+    def criarQrcode(link):
         img = qrcode.make(link)
+        
+        createQrCode.salvarQrCode(img)
+
+    def tratamentoNome(name):
+        name = re.sub(r"\s", "_", name)
+        name = re.sub(r'[<>:"/\\|?*]', '', name)
+        name = name.replace("'", "")
+        return name
+
+        
+    
+    def salvarQrCode(img, name):
+        output_path = '_qrcodes'
+
+        if not os.path.exists(output_path, name):
+            os.makedirs(output_path)
+            print('Diretório criado com sucesso!')
+
+        
 
         img.save(f'{name}.png')
+
+
+        return name
 
 if __name__ == "__main__":
     
